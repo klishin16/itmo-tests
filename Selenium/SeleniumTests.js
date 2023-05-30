ba2ca8fe1a15f29d8a35c28f1e875ccd8e20d349
Max Chistyakov <chistyakovmaxim2000@gmail.com>
2023-05-30 21:44:28 +0300

    add https://pomofocus.io/ test

--- Selenium/SeleniumTests.js
+++ Selenium/SeleniumTests.js
@@ -1,12 +1,18 @@
 const { Builder, By, Key } = require('selenium-webdriver');
 
 async function example() {
+    // Создаём инстанс вебдрайвера
     let driver = await new Builder().forBrowser('chrome').build();
 
+    //переходим на страницу
     await driver.get('https://www.google.com/');
-    await driver
-        .findElement(By.name('q'))
-        .sendKeys('Selenium WebDriver', Key.RETURN);
+
+    // Получаем элемент ввода
+    const input = driver.findElement(By.name('q'));
+
+    // Вызываем на элементе события нажатия клавиш (ввод в поиск и Enter)
+    await input.sendKeys('Selenium WebDriver', Key.RETURN);
+
 
     await driver.quit();
 }
